a108b9a505a1108737dc8ee1a681fd9f1cabdd7b
RohitNag11 <rnag2018@gmail.com>
2021-02-19 22:25:43 +0000

    Revert "Delete nbproject directory"
    This reverts commit dc7baa145265cf86daed085be7e099cac0b91970.

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Auto-Lamp.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Auto-Lamp.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=auto-lamp.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/auto-lamp.x.tar
